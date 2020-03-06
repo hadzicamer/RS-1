6987e951deee52ef72a92788eabb53260c3bc8f8
Amer Hadžić <amerbu_98@hotmail.com>
2020-03-06 15:26:30 +0100

    Test

---
+++ RS1_Ispit_2020_01_30_aspnet_core/RS1_Ispit/EntityModels/TakmicenjeUcesnik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS1_Ispit_asp.net_core.EntityModels
+{
+    public class TakmicenjeUcesnik
+    {
+        public int TakmicenjeUcesnikID { get; set; }
+        public int OdjeljenjeStavkaID { get; set; }
+        public virtual OdjeljenjeStavka OdjeljenjeStavka { get; set; }
+        public bool isPristupio { get; set; }
+        public int bodovi { get; set; }
+        public int TakmicenjeID { get; set; }
+        public virtual Takmicenje Takmicenje { get; set; }
+    }
+}
